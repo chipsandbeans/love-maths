a746385be72d0b696953dc01344c55a1e1d2b046
Byran Murphy <bryanmurphy401@gmail.com>
2024-10-16 13:28:35 +0000

    added division game to the calculator

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,6 +41,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2)
     } else {
         alert(`Unknown Game Type: ${gameType}`);
         throw `Unknown Game Type: ${gameType}.Aborting!`
@@ -82,7 +84,10 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else {
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+    }
+        else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}.Aborting!`;
     }
@@ -124,6 +129,16 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
 
+function displayDivisionQuestion(operand1, operand2) {
+
+    operand1 = operand1 * operand2;
+
+    document.getElementById("operand1").textContent = operand1;
+
+    document.getElementById("operand2").textContent = operand2;
+
+    document.getElementById("operator").textContent = "/";
 
 }
